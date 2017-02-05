ddace0d4ec52ffca6f3324ecd18c23cdb4353132
meganetaaan <ishikawa.s.1027@gmail.com>
2017-02-05 23:38:11 +0900

    added adder(test)

---
+++ adder/src/lib.rs
@@ -0,0 +1,23 @@
+//! `adder` crate provides a function that adds a number to another.
+
+/// This function adds two to the argument.
+/// #Examples
+///
+/// ```
+/// use adder::add_two;
+///
+/// assert_eq!(4, add_two(2));
+/// ```
+pub fn add_two(a: i32) -> i32 {
+    a + 2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(4, add_two(2));
+    }
+}
