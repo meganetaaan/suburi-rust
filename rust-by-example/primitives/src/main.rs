917a65bd324ca52a632bf1de8e06f41bf9a4cf00
meganetaaan <ishikawa.s.1027@gmail.com>
2017-02-12 23:43:54 +0900

    done 2.1 Literals and operators

---
+++ rust-by-example/primitives/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // Variables can be annotated
+    let logical: bool = true;
+
+    let a_float: f64 = 1.0;
+    let an_integer = 5i32;
+
+    // Or a default will be used.
+    let default_float = 3.0; // f64
+    let default_integer = 7; // i32
+
+    let mut mutable = 12;
+    mutable = 7;
+}
