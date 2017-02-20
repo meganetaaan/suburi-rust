bb2b463d06ae6872b5fdca8396e4b58b9dd7b7f0
meganetaaan <ishikawa.s.1027@gmail.com>
2017-02-20 11:00:04 +0900

    done 4.3 declare first

---
+++ rust-by-example/variable-bindings/src/declare_first.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let a_binding;
+
+    {
+        let x = 2;
+        a_binding = x * x;
+    }
+
+    println!("a binding: {}", a_binding);
+
+    let another_binding;
+
+    // println!("another_binding: {}", another_binding);
+
+    another_binding = 1;
+
+    println!("another_binding: {}", another_binding);
+}
