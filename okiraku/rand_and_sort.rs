25e30ab75e4138a2d613d32b64feca0bf7b24f90
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2017-07-30 00:29:01 +0900

    Add quick sort

--- okiraku/rand_and_sort.rs
+++ okiraku/rand_and_sort.rs
@@ -73,6 +73,24 @@ fn insert_sort1<T: Ord + Copy>(buff: &mut [T]) {
     }
 }
 
+fn quick_sort<T: Ord + Copy>(buff: &mut [T]) {
+    if buff.len() < 2 { return; }
+    let pivot = buff[buff.len() / 2];
+    let mut i = 0;
+    let mut j = buff.len() - 1;
+    loop {
+        while pivot > buff[i] { i += 1; }
+        while pivot < buff[j] { j -= 1; }
+        if i >= j { break; }
+        buff.swap(i, j);
+        i += 1;
+        j -= 1;
+    }
+    if i > 0 { quick_sort(&mut buff[.. i]); }
+    if j < buff.len() - 1 { quick_sort(&mut buff[j + 1 ..]); }
+}
+
+
 fn test(func: fn(&mut [i32]) -> (), rng: &mut Rand) {
     let mut buff: [i32; 20] = [0; 20];
     for i in 0 .. 20 { buff[i] = i as i32; }
@@ -98,4 +116,6 @@ fn main() {
     test(insert_sort, &mut rng);
     println!("----- insert sort 1 -----");
     test(insert_sort1, &mut rng);
+    println!("----- quick sort -----");
+    test(quick_sort, &mut rng);
 }
