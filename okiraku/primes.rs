9acfabf47ed6af235eebfc8cd546ceb1746d898f
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2017-08-21 21:01:47 +0900

    Done twin primes

--- okiraku/primes.rs
+++ okiraku/primes.rs
@@ -39,6 +39,40 @@ impl Iterator for Primes {
     }
 }
 
+// TwinPrimes
+struct TwinPrimes {
+    ps: Primes,
+    prev: i32,
+}
+
+impl TwinPrimes {
+    fn new() -> TwinPrimes {
+        TwinPrimes {
+            ps: Primes::new(),
+            prev: 1,
+        }
+    }
+    fn reset(&mut self) {
+        self.ps.reset();
+        self.prev = 1;
+    }
+}
+
+impl Iterator for TwinPrimes {
+    type Item = (i32, i32);
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let p = self.ps.next().unwrap();
+            if p - self.prev == 2 {
+                let q = self.prev;
+                self.prev = p;
+                return Some((q, p));
+            }
+            self.prev = p;
+        }
+    }
+}
+
 fn main() {
     let mut ps = Primes::new();
     for _ in 0..25 {
@@ -50,4 +84,15 @@ fn main() {
         print!("{} ", ps.next().unwrap());
     }
     println!("");
+
+    let mut twinps = TwinPrimes::new();
+    for _ in 0..100 {
+        print!("{:?}", twinps.next().unwrap());
+    }
+    println!("");
+    twinps.reset();
+    println!(
+        "{:?}",
+        twinps.take_while(|x| x.1 < 3_000_000).last().unwrap()
+    );
 }
